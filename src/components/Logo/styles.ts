2eb3ee5c46340d739479587a743b806e6b0de73f
Lucas Dias <lucasdiasrodrigues@hotmail.com>
2020-10-25 01:38:26 -0300

    Add option to Logo on Mobile

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 import { LogoProps } from '.'
 
@@ -11,13 +12,30 @@ const wrapperModifiers = {
   large: () => css`
     height: 5.9rem;
     width: 20rem;
+  `,
+
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      height: 4.5rem;
+      width: 5.8rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
   `
 }
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
 
     ${!!size && wrapperModifiers[size]}
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile}
   `}
 `
